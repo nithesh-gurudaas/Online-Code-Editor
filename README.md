f7be50506b3dad6278112bd12b9318729992f9f1
Nithesh Gurudaas <59699356+nithesh-gurudaas@users.noreply.github.com>
2021-07-17 18:08:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+## Getting Started
+
+1. Clone this repository 
+2. `cd` into it and use the command `npm install` to download dependencies.
+3. Run `node server.js` to start the Express server.
+4. `cd` into the client folder, run `npm install` followed by `npm start` to start the development server. Open http://localhost:3000 in your browser.
+
+## Pre-requisites
+
+- [Node.js](https://nodejs.org/en) and npm
+
+## Built With
+
+- [React](https://reactjs.org)
+- [Pusher Channels](https://pusher.com/channels)
---
+++ client/src/App.js
@@ -0,0 +1,156 @@
+import React, { Component } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import Pusher from "pusher-js";
+import pushid from "pushid";
+import axios from "axios";
+import PasteClient from "pastebin-api";
+import "./App.css";
+import "codemirror/lib/codemirror.css";
+import '../node_modules/codemirror/mode/javascript/javascript.js';
+
+import Tabs from "./components/Tabs"; 
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript.js";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      html: "",
+      css: "",
+      js: ""
+    };
+
+    this.pusher = new Pusher("5526b96acf90e40787a92", {
+      cluster: "ap2",
+      forceTLS: true
+    });
+
+    this.channel = this.pusher.subscribe("editor");
+  }
+  
+
+  componentDidUpdate() {
+    this.runCode();
+  }
+
+  componentDidMount() {
+    this.setState({
+      id: pushid()
+    });
+
+    this.channel.bind("text-update", data => {
+      const { id } = this.state;
+      if (data.id === id) return;
+
+      this.setState({
+        html: data.html,
+        css: data.css,
+        js: data.js
+      });
+    });
+  }
+
+  syncUpdates = () => {
+    const data = { ...this.state };
+
+    axios
+      .post("http://localhost:5000/update-editor", data)
+      .catch(console.error);
+  };
+  
+  runCode = () => {
+    const { html, css, js } = this.state;
+
+    const iframe = this.refs.iframe;
+    const document = iframe.contentDocument;
+    const documentContents = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+
+        <script type="text/javascript">
+          ${js}
+        </script>
+      </body>
+      </html>
+    `;
+    
+    document.open();
+    document.write(documentContents);
+    document.close();
+  };
+    
+  render() {
+    const { html, js, css } = this.state;
+    const codeMirrorOptions = {
+      theme: "default",
+      lineNumbers: true,
+      scrollbarStyle: null,
+      lineWrapping: true
+    };
+
+    return (
+      <div className="App">
+        <section className="playground" id="codeeditor1">
+        <Tabs>
+
+          <div label="index.html">
+            <CodeMirror
+              value={html}
+              options={{
+                mode: "htmlmixed",
+                ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, html) => {
+                this.setState({ html }, () => this.syncUpdates());
+               
+              }}
+            />
+          </div>
+          <div label="index.css">
+            <CodeMirror
+              value={css}
+              options={{
+                mode: "css",
+                ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, css) => {
+                this.setState({ css }, () => this.syncUpdates());
+              }}
+            />
+          </div>
+          <div label="index.js">
+            <CodeMirror
+              value={js}
+              options={{
+                mode: "javascript",
+                ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, js) => {
+                this.setState({ js }, () => this.syncUpdates());
+              }}
+            />
+          </div>
+          </Tabs>
+        </section>
+        <section className="result">
+          <iframe title="result" className="iframe" ref="iframe" />
+        </section>
+      </div>
+    );
+  }
+}
+export default App;
